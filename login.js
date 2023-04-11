e77efb2bb177c0cae57767869771621504e0d2f8
Tiankun Xu <124283962+Tiankun23@users.noreply.github.com>
2023-04-10 21:56:25 -0400

    Add files via upload

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 var back=document.getElementById('back');
 window.onmousemove = function(event){
-    var x=-event.clientX/10;
-    var y=-event.clientY/15;
+    var x=-event.clientX/25;
+    var y=-event.clientY/30;
     back.style.backgroundPositionX = x+"px";
     back.style.backgroundPositionY = y+"px";
 }
@@ -20,15 +20,5 @@ function login(){
     }
 }
 
-var con=document.getElementById('con')
-function loadoff(){
-    con.style.display="none";
-}
-function loadon(){
-    con.style.display="flex";
-}
-window.onload=function(){
-    loadon();
-    setTimeout(loadoff,3000);
-}
+
 
